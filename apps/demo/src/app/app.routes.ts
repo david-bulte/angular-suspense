4e57a70a35ab51457c8fe947f0894b5afa680990
david-bulte <david.bulte@gmail.com>
2022-02-26 16:28:05 +0100

    remove item-component

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import {
-  ItemComponent,
   MovieDetailComponent,
   MoviesComponent,
 } from '@angular-suspense/demo-feature-movies';
@@ -22,81 +21,8 @@ export const routes: Routes = [
     ],
   },
   {
-    path: 'item-1',
-    component: ItemComponent,
-    data: {
-      debug: 'item-1',
-      links: ['item-1.1', 'item-1.2', 'theatre'],
-      wait: 200,
-      error: false,
-      hasChild: false,
-      hasLocalLoadingState: false,
-      catchError: false,
-      stopPropagation: false,
-    },
-    children: [
-      {
-        path: 'item-1.1',
-        component: ItemComponent,
-        data: {
-          debug: 'item-1.1',
-          links: ['item-1.1.1'],
-          wait: 100,
-          error: false,
-          hasChild: true,
-          hasLocalLoadingState: false,
-          catchError: false,
-          stopPropagation: false,
-        },
-        children: [
-          {
-            path: 'item-1.1.1',
-            component: ItemComponent,
-            data: {
-              debug: 'item-1.1.1',
-              links: [],
-              wait: 1000,
-              error: false,
-              hasChild: false,
-              hasLocalLoadingState: false,
-              catchError: false,
-              stopPropagation: false,
-            },
-          },
-        ],
-      },
-      {
-        path: 'item-1.2',
-        component: ItemComponent,
-        data: {
-          debug: 'item-1.2',
-          links: [],
-          wait: 400,
-          error: false,
-          hasChild: false,
-          hasLocalLoadingState: false,
-          catchError: false,
-          stopPropagation: false,
-        },
-      },
-    ],
-  },
-  {
     path: '',
-    // redirectTo: 'item-1',
     redirectTo: 'movies',
     pathMatch: 'full',
   },
 ];
-// export const routes: Routes = [{
-//   path: 'movies',
-//   component: MoviesComponent,
-//   children: [{
-//     path: ':name',
-//     component: MovieDetailComponent
-//   }]
-// }, {
-//   path: '',
-//   redirectTo: 'movies',
-//   pathMatch: 'full'
-// }];
--- libs/demo/feature-movies/src/lib/demo-feature-movies.module.ts
+++ libs/demo/feature-movies/src/lib/demo-feature-movies.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SuspenseModule } from '@david-bulte/angular-suspense';
 import { AvatarComponent } from './avatar/avatar.component';
-import { ItemComponent } from './item/item.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MoviesComponent } from './movies/movies.component';
@@ -13,10 +12,9 @@ import { MoviesComponent } from './movies/movies.component';
   declarations: [
     MoviesComponent,
     MovieDetailComponent,
-    ItemComponent,
     AvatarComponent,
     MovieCardComponent,
   ],
-  exports: [MoviesComponent, MovieDetailComponent, ItemComponent],
+  exports: [MoviesComponent, MovieDetailComponent],
 })
 export class DemoFeatureMoviesModule {}
